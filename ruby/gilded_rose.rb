4849e2ed2e0ad500fd191cfbe6c2489c43b6f66e
LinnikV <linnikvitek@ukr.net>
2024-05-24 13:34:22 +0300

    refactoring gilded_rose

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,55 +1,89 @@
 class GildedRose
-
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      item_updater(item).call(item)
     end
+  end
+
+  private
+
+  def item_updater(item)
+    case item.name
+    when /Aged/
+      AgedItem
+    when /Sulfuras/
+      SulfurasItem
+    when /Backstage passes/
+      BackstageItem
+    when /Conjured/
+      ConjuredItem
+    else
+      StandartItem
+    end
+  end
+end
+
+class ItemUpdater 
+  def self.sell_in_down(item, index = 1)
+    item.sell_in -= index
+  end
+
+  def self.quality_up(item, index = 1)
+    index *= 2 if item.sell_in < 0
+    item.quality += index
+  end
+
+  def self.quality_down(item, index = 1)
+    index *= 2 if item.sell_in < 0
+    item.quality -= index
+  end
+
+  def self.check_limit(item)
+    item.quality = 0 if item.quality < 0
+    item.quality = 50 if item.quality > 50
+  end
+end
+
+class AgedItem < ItemUpdater
+  def self.call(item)
+    sell_in_down(item)
+    quality_up(item)
+    check_limit(item)
+  end
+end
+
+class SulfurasItem < ItemUpdater
+  def self.call(item); end
+end
+
+class BackstageItem < ItemUpdater
+  def self.call(item)
+    sell_in_down(item)
+    quality_up(item) if item.sell_in > 11
+    quality_up(item, 2) if item.sell_in < 11
+    quality_up(item, 3) if item.sell_in < 6
+    item.quality = 0 if item.sell_in < 0
+    check_limit(item)
+  end
+end
+
+class ConjuredItem < ItemUpdater
+  def self.call(item)
+    sell_in_down(item)
+    quality_down(item, 2)
+    check_limit(item)
+  end
+end
+
+class StandartItem < ItemUpdater
+  def self.call(item)
+    sell_in_down(item, index = 1)
+    quality_down(item, index = 1)
+    check_limit(item)
   end
 end
 
